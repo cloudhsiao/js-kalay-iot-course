2aded7788533b669f7c2884ab7f14a95906fb143
cloudhsiao <cloudhs@gmail.com>
2016-04-29 07:52:45 +0000

    add thingspeak support fix bling-led code

--- course-bling-led_bbb.js
+++ course-bling-led_bbb.js
@@ -2,17 +2,13 @@ var Gpio = require('onoff').Gpio,
     led = new Gpio(68, 'out'),
     iv;
 
-function exit() {
-    led.unexport();
-    process.exit();
-}
-
 iv = setInterval(function () {
     led.writeSync(led.readSync() ^ 1);
 }, 200);
 
-setTimeout(function () {
+process.on('SIGINT', function() {
     clearInterval(iv);
     led.writeSync(0);
     led.unexport();
-}, 10 * 1000);
+    process.exit();
+});
---
+++ course-dht11-thingspeak_bbb.js
@@ -0,0 +1,29 @@
+var dht = require('beaglebone-dht'),
+    request = require('request'),
+    format = require('string-format'),
+    sensor = dht.sensor('DHT11'),
+    pin = 'P9_11',
+    iv,
+    data;
+
+var api_key = 'ZBS7S0PEKP3217KD';
+
+format.extend(String.prototype);
+
+iv = setInterval(function() {
+    var data = dht.read(pin);
+    if (data) {
+        console.log(data);
+        var url = 'https://api.thingspeak.com/update?api_key={0}&field1={1}&field2={2}'.format(api_key, data.celsius, data.humidity);
+        request(url, function(err, resp, body) {
+            if (err) {
+                throw err;
+            }
+            console.log('resp: ' + JSON.stringify(resp) + ', body: ' + body);
+        })
+    }
+}, 10000);
+
+process.on('SIGINT', function() {
+    clearInterval(iv);
+});
